3ffda4b5e1383b16cb3150ed99e562b73994a845
yejinleee <81412212+yejinleee@users.noreply.github.com>
2021-08-19 17:36:02 +0900

    Add files via upload

--- App.js
+++ App.js
@@ -6,47 +6,35 @@ import Toggle from './Toggle';
 
 function App() {
 
-    const [district,setDistrict] = useState(['경기도','강원도','서울특별시','충청북도','충청남도','전라북도','전라남도']);
-    const [toggleopen,setToggleopen ] =useState(false);
+  const [district,setDistrict] = useState(['경기도','강원도','서울특별시','충청북도','충청남도','전라북도','전라남도']);
 
-    const toggleMenu = () =>{
-        setToggleopen(!toggleopen);
-    }
+  const [toggleOpen, setToggleOpen] = useState(false);
 
-  return (
-    <div className="App">
-      <header className="App-header">
-        <div> U SPOT </div>
-          <div>
-              <a
-                  className="App-link"
-                  href="https://www.notion.so/1200gang/2021-8019c43d60864002b32af6b52eb5872d"
-                  target="_blank"
-                  rel="noopener noreferrer"
-              >
-                  광광이 노션 바로가기
-              </a>
-          </div>
-      </header>
-      <div className="list">
-          <ul>
-              {district.map((v) => {
-                  return(
-                      <>
-                          <li onClick={toggleMenu}> {v} </li>
-                          <div>
-                              {
-                                toggleopen ? <Toggle dst={v}></Toggle> : null
-                              }
-                          {/*<Toggle></Toggle>*/}
-                            </div>
-                      </>
-                  );
-              })}
-          </ul>
+  
+  return(
+    <div className="list">
+          <Toggle></Toggle>
       </div>
-    </div>
+
   );
+
 }
 
 export default App;
+
+
+
+// <div>
+// {
+//   toggleopen ? <Toggle dst={v}></Toggle> : null
+// }
+// {/*<Toggle></Toggle>*/}
+// </div>
+
+
+// li 경기도  <Toggle>
+
+// li 강원도
+
+// <Toggle>
+//   누르면 리스트
--- Toggle.jsx
+++ Toggle.jsx
@@ -1,30 +1,74 @@
 import React, {memo, useMemo,useState} from 'react';
 
-const Toggle = memo(({dst}) => {
-    const [isToggleOn, setToggleOn] = useState(true);
-    const 경기도 = ['구리', '수원', '의정부']
+const Toggle =() =>{
 
-    const handleClick = () => {
-        setToggleOn(!isToggleOn);
+    const [district,setDistrict] = useState(['경기도','강원도','서울특별시','충청북도','충청남도','전라북도','전라남도']);
+
+    const [gyeongi, setGyeongi] = useState(['구리', '수원', '의정부']);
+    const [gangwon, setGangwon] = useState(['춘천','동해','양양']);
+
+    const onChangeGyeongi = (e) =>{
+        setGyeongi(e.target.value);
     };
-    // console.log(경기도)
+    const onChangeGangwon = (e) => {
+        setGangwon(e.target.won);
+    };
+    
+    function showcities(){
+        {gyeongi.map((v) => {
+            return (
+                <li> {v}</li>
+            )
+        })}
+    }
 
-    return (
-        <div className="toggle">
-            <ul className={isToggleOn ? "show-menu" : "hide-menu"}>
-                {dst === '경기도' ? 경기도.map((v) => {
-                    return (
-                        <>
-                            <li>{v}</li>
-                        </>
-                    )
-                }) : null}
+    return(
+        <>
+            <ul>
+              <li onClick={ () => {showcities}}> 경기도 </li>   //함수로
+              <li onClick={ () => {gangwon}}> 강원도 </li>      //cosnt로
             </ul>
-        </div>
-        // <button onClick={handleClick}>
-        //     {isToggleOn ? {a} : '🔽'}
-        // </button>
-    );
-})
+
+        </>
+    )
+
+}
+
+
+
+// const Toggle = memo(({dst}) => {
+//     const [isToggleOn, setToggleOn] = useState(true);
+//     const 경기도 = useState(['구리', '수원', '의정부']);
+
+//     const handleClick = () => {
+//         setToggleOn(!isToggleOn);
+//     };
+//     // console.log(경기도)
+
+//     return (
+//         <div className="toggle">
+//             <ul className={isToggleOn ? "show-menu" : "hide-menu"}>
+//                 {dst === '경기도' ? 경기도.map((v) => {
+//                     return (
+//                         <>
+//                             <li>{v}</li>
+//                         </>
+//                     )
+//                 }) : null}
+//             </ul>
+//         </div>
+//         // <button onClick={handleClick}>
+//         //     {isToggleOn ? {a} : '🔽'}
+//         // </button>
+//     );
+// })
+
+
+
+
+// <ul>
+// <li onChange={onChangeGyeongi}>{gyeongi}</li>
+// <li onChange={onChangeGangwon}>{gangwon}</li>
+// </ul>
 
 export default Toggle;
